c23e1430ca913db0ab7fedbbeb7af4b745d4067c
ValentinHer <valentinhernandezhernandez18@gmail.com>
2025-06-05 17:08:46 -0600

    [ADD] Se crea layout principal en app.jsx, se instalo bootstrap y bootstrap-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 function App() {
   return (
     <>
-      <h1>Welcome to My React App</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1 className="h1 text-center mt-2 bg-danger" >Welcome to My React App</h1>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
